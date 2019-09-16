61ebd716aec7c7d451dfbfd0ee1de963527a5a77
Ponnuru Venkatesh <ponnuruvenkatesh@ponnurus-mbp.lan>
2019-09-16 14:49:39 -0700

    added vpc module

--- main.tf
+++ main.tf
@@ -1,35 +1,76 @@
-provider "aws" {
-access_key = "${var.access_key}"
-secret_key = "${var.secret_key}"
- region = "${var.region}"
+:provider "aws" {
+ access_key = "${var.access_key}"
+ secret_key = "${var.decret_key}"
+ region = "us-east-2"
 }
-resource "aws_vpc" "tata-vpc" {
- 
- cidr_block = "${var.cidr_block}"
+terraform {
+ backend "s3"{
+ bucket = "terraform-storage-statefiles-f3"
+ key = "myapp/dev/terrafor.tfstatefile"
+ profile = "terraform-user"
+ region = "us-east-2"
+}
+}
+resource "aws_vpc" "TATA-vpc"{
+ cidr_block = "10.50.0.0/16"
  instance_tenancy = "default"
- enable_dns_hostname = "true"
- tags{
-  Name = "TATA_VPC_1"
-  }
- }
-resource "aws_subnet" "tata-subnet"{
- vpc_id = "${aws_vpc.tata-vpc.id}"
- cidr_block = "${var.subnet_cidr}"
- tags{
-  Name = "TATA_subnet_1"
-  }
- }
-resource "aws_internet_gateway" "i_g_w" {
- vpc_id = "${aws_vpc.tata_vpc.id}"
- tags{
-  Name = "TATA_INTERNET_GATEWAY"
-  }
- }
-resource "aws_instance"  "main" {
-ami = "ami-0c55b159cbfafe1f0"
-instance_type = "t2.micro"
-key_name = ""
-tags{
- Name = "first-intsance-using-TF"
+ enable_dns_hostnames = true
+tags {
+ Name = "TATA-vpc"
 }
+}
+resource "aws_subnet" "tata-subnet"{
+ vpc_id = "${aws_vpc.TATA-vpc.id}
+ cidr_block = "10.50.1.0/24"
+tags{
+ Name = "tata-subnet"
+}
+}
+resource "aws_internet_gateway" "igw-tata"{
+ vpc_id = "${aws_vpc.TATA-vpc.id}"
+tags{
+ Name = "igw-for-tata-server"
+}
+}
+resource "aws_route_table" "route-tata-server"{
+ vpc_id = "${aws_vpc.TATA-vpc.id}"
+ route{
+  cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.igw-tata.id}"
+}
+tags {
+ Name = "tata-server-RT"
+}
+}
+resource "aws_route_table_asoociation" "route-tata-server"{
+ subnet_id = "${aws_subnet.tata-subnet.id}"
+ route_id = "${aws_route_table.route-tata-server.id}"
+}
+resource "aws_security_group" "tata-sg" {
+  Name = "security-group-tata"
+  description = "allow incoming http and ssh connection"
+  vpc_id = "${aws_vpc.TATA-vpc.id}"
+ ingress {
+  from_port = "0"
+  to_port = "65535"
+  cidr_blocks = ["0.0.0.0/0"]
+  protocol = "tcp"
+}
+ tags {
+  Name = "webserver-sec-grp"
+} 
+}
+resource "aws_instance" "main"{
+ ami = "ami-0c55b159cbfafe1f0"
+ instance_type = "t2.micro"
+ vpc_id = "${aws_vpc.TATA-vpc.id}"
+ subnet_id = "${aws_subnet.tata-subnet.id}"
+ vpc_security_group_ids = ["${aws_security_group.tata-sg.id}"]
+ associate_public_ip_address = true
+ key = "awslabvenkatohio"
+}
+tags{
+ Name = "webserver"
+}
+
 
